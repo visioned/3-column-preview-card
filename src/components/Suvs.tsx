9d552bcbf1eb41809a3b0c5817105005deca5730
visioned <ajkg11@gmail.com>
2023-02-20 15:57:28 -0800

    asdf

--- src/components/Suvs.tsx
+++ src/components/Suvs.tsx
@@ -9,7 +9,7 @@ export const Suvs = () => {
         </h1>
         <p className="mt-[28px] mr-8 text-[15px] leading-[24px] lg:tracking-wider lg:mr-[2px]">
           Take an SUV for its spacious interior, power, and versatility. Perfect
-          for your next family vacation and &nbsp;&nbsp; off-road adventures.  
+          for your next family vacation and off-road adventures.  
         </p>
         <button className="h-[48px] w-[147px] bg-[#fff] rounded-full mt-[28px] lg:mt-[86px] text-[#016972] hover:border-2 hover:border-white hover:bg-[#016972] hover:text-white ">
           Learn More
